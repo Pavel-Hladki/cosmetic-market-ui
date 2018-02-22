c9614ef7e7b813250f45829c6084bd4950d5ec82
y_tataritskaya <j.tataritskaya@vironit.co.uk>
2018-02-22 23:47:26 +0300

    added form for creating new product

---
+++ src/app/components/shared/classes/reactive-forms.base.class.ts
@@ -0,0 +1,23 @@
+import {FormGroup} from '@angular/forms';
+export abstract class ReactiveFormsBaseClass {
+  constructor(protected formErrors: any, protected validationMessages: any) {}
+
+  protected onValueChanged (form: FormGroup, data?: any): void {
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
