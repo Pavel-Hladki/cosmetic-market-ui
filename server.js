84c15e61fc1bd1bd136476a6c7b8a4004787589a
Pavel Hladki <pavel.gladkiy@gmail.com>
2018-03-11 21:32:56 +0300

    server fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html')
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
