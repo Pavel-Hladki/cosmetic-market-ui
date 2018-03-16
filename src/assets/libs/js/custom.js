8d0a41e415abb50fe9b16c371356ed55976b0681
pavelhladki <phladki@objectstyle.com>
2018-03-16 13:34:35 +0300

    fix for menu

--- src/assets/libs/js/custom.js
+++ src/assets/libs/js/custom.js
@@ -15,7 +15,20 @@
 
   $(window).resize(function() {
       resizeLogo();
+      manageMenuCollapsing();
   });
+
+  function manageMenuCollapsing() {
+    const $menuItems = $(".navigation-bar-main-collapse ul li");
+
+    if($(window).width() < 750 ) {
+      $menuItems.attr("data-toggle", "collapse");
+      $menuItems.attr("data-target", ".navigation-bar-main-collapse");
+    } else {
+      $menuItems.removeAttr("data-toggle");
+      $menuItems.removeAttr("data-target");
+    }
+  }
 
   function resizeLogo() {
       if(($(window).width() < 750 && $(".navigation-bar").offset().top > 50)
@@ -23,26 +36,26 @@
           || ($(window).width() < 750 && $(".navigation-bar").offset().top < 50)){
           $(".navigation-bar-brand > img.aloe-logo").hide();
           $(".navigation-bar-brand > img.aloe-logo-min").show();
-          //$(".navigation-bar-brand").prepend('<img class="aloe-logo-min" src="assets/img/icons/logoAloe-min-white.png">');
       } else {
           $(".navigation-bar-brand > img.aloe-logo-min").hide();
           $(".navigation-bar-brand > img.aloe-logo").show();
-          //$(".navigation-bar-brand").prepend('<img class="aloe-logo" src="assets/img/icons/logoAloe-white.png">');
       }
   }
 
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
     resizeLogo();
+    manageMenuCollapsing();
 
 		$('.navigation-bar-nav li a').on('click', function(event) {
 
-			if( $(this).is('a:not([href^="#"])') || $(this).attr("href") == '#' ) {
+			if( ($(this).is('a:not([href^="#"])') && $(this).is('a:not([href^="/#"])'))
+        || ($(this).attr("href") == '#' && $(this).attr("href") == '/#')) {
 	     return;
 	    }
-			var $anchor = $(this);
+			var $anchorRef = $(this).attr('href');
 			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
+				scrollTop: $($anchorRef.substring($anchorRef.indexOf("#"), $anchorRef.length)).offset().top
 			}, 1500, 'easeInOutExpo');
 			event.preventDefault();
 		});
